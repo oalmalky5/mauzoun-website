c4efb6562d8f34370de6306e75c15438a4a6e4e3
oalmalky5 <oalmalky5@gmail.com>
2021-04-10 14:34:34 +0300

    update

--- pages/main.js
+++ pages/main.js
@@ -26,9 +26,9 @@ export default function Home() {
         <div className={styles.logo}><Link href="main" ><img src="https://i.imgur.com/0IJeIP7.png" alt="Mauzoun logo"/></Link></div>
         <div className={styles.navLinks}>
             <Link href="story">Story</Link>
-            <Link href="services">Services</Link>
-            <Link href="portfolio">Portfolio</Link>
-            <Link href="job">Mauzoun & You</Link>
+            <Link classname={styles.serviceLink} href="services">Services</Link>
+            <Link className={styles.protLink} href="portfolio">Portfolio</Link>
+            <Link className={styles.jobLink} href="job">Mauzoun & You</Link>
         </div>
         <div className={styles.bottomNavIcons}>
             <div className={styles.twitter}><a target="_blank" href='https://twitter.com/mauzoun_?lang=en'><IoLogoTwitter size='30px'/></a></div>
@@ -54,15 +54,16 @@ export default function Home() {
             <span className={styles.text1}>Our Services</span>
             {button1Show ? 
                 <span onClick={() => setButton1Show(false)}><BsChevronDown className={styles.icon1}/></span> : 
-            <span> include developing strategic content for brands and campaigns, as well as unleashing our creativities in book editing, creative writing, story doctoring, and boutique publishing.<div><hr/></div></span> }
+            <span> include developing strategic content for brands and campaigns, as well as unleashing our creativities in book editing, creative writing, story doctoring, and boutique publishing. <hr/></span>  }
         </div>
+       
     </div> 
     
     <div className={styles.approach}>
         <div> 
             <span className={styles.text2}>Our approach to writing is thoughtful and imaginative</span> 
             {button2Show ? <span onClick={() => setButton2Show(false)}><BsChevronDown className={styles.icon1}/></span> : 
-            <span>, balancing between Arabic and English while remaining loyal to each language and the kind of service we provide. We believe in work that is backed by a strong understanding of brand strategies, audience needs, and empathetic storytelling.</span> }
+            <span>, balancing between Arabic and English while remaining loyal to each language and the kind of service we provide. We believe in work that is backed by a strong understanding of brand strategies, audience needs, and empathetic storytelling.<hr/></span> }
         </div>
     </div>
 
@@ -70,7 +71,7 @@ export default function Home() {
         <div> 
             <span className={styles.text3}>We seek projects that need us as much as we need them</span> 
             {button3Show ? <span onClick={() => setButton3Show(false)}><BsChevronDown className={styles.icon1}/></span> : 
-            <span>, from innovative brands to independent authors with passion projects. You can read our tales of success in our portfolio.</span> }
+            <span>, from innovative brands to independent authors with passion projects. You can read our tales of success in our portfolio.<hr/></span> }
         </div>
     </div>
 
@@ -78,7 +79,7 @@ export default function Home() {
         <div> 
             <span className={styles.text4}>Our work is dedicated and ethical</span> 
             {button4Show ? <span onClick={() => setButton4Show(false)}><BsChevronDown className={styles.icon1}/></span> : 
-            <span>, prioritizing our community as much as our clients. This is why we host an annual competition for up-and-coming writers who are developing their first-ever manuscripts. Winners are provided with free editing, marketing, and publishing services to unleash their works to the world.</span> }
+            <span>, prioritizing our community as much as our clients. This is why we host an annual competition for up-and-coming writers who are developing their first-ever manuscripts. Winners are provided with free editing, marketing, and publishing services to unleash their works to the world.<hr/></span> }
         </div>
     </div>
 
