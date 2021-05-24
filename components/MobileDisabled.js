6f3557bde909bc05e8ea783647f0cf1e49216a6a
oalmalky5 <oalmalky5@gmail.com>
2021-05-24 14:56:29 +0300

    fixing mobile view

--- components/MobileDisabled.js
+++ components/MobileDisabled.js
@@ -7,7 +7,7 @@ const MobileDisabled=() => <div className={styles.mobileNotsupportedContainer}>
     لا يزال موقعنا في مرحلته التجريبية، ولذلك فهو لا يدعم التصفح عبر الهاتف المحمول. يرجى استخدام متصفح حاسوبكم للاطلاع على الموقع بأفضل شكل.
     </h6>
     <p className={styles.mobileNotsupportedTextEn}>
-    Our website is currently still in its beta version, and therefore does not support mobile view. For the best experience, please view the website from a desktop browser.
+    Our website is currently still in its beta version, <br/>and therefore does not support mobile view. <br/>For the best experience, please view the website <br/>from a desktop browser.
     </p>
     <img className={styles.logo} src='/Mauzoun – Transparent Logo – Black-01.png' alt='Mauzoun logo'/>
 </div>
