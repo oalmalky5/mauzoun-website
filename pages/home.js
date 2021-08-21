eb1db0f07b7e503af515281edad821814a547872
oalmalky5 <oalmalky5@gmail.com>
2021-08-21 22:30:28 +0300

    fixing pic

--- pages/home.js
+++ pages/home.js
@@ -90,7 +90,7 @@ export default function Home({ updatePageTransition, textAnimationControls, hand
                   height='400px'
                   layout='fixed'
                   priority='true'
-                  src='/team/Website-photo-01.png'
+                  src='mainOffice.png'
                   alt='an image of an office'
                   transition={{ duration: 0.5 }}
                 />
