9419757f6e34732745e92e02464258b163c7d154
subhankhalid402 <subhan.khalid402@gmail.com>
2021-10-06 13:49:39 +0500

    Musa & palm image change

--- pages/musaandpalm.js
+++ pages/musaandpalm.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {useIntl} from "react-intl";
 import {useRouter} from "next/router";
 import {motion} from "framer-motion";
@@ -110,7 +111,8 @@ export default function musaandpalm({textAnimationControls, handleBgColorChange,
                             </div>
 
                             <div>
-                                <img className={styles.image2} src="Musa&PalmEN2.png" alt="Musa & Palm table"/>
+                                <img className={styles.image2} src={locale !== "ar" ? "Musa&PalmEN2.png":"musa&palmar.png"}
+                                     alt="Musa & Palm table"/>
                                 <span className={styles.para1}>{f("process.para1")}</span>
                             </div>
 
