717a3d1deed01e37e4e4114daffe4265b5613d9b
subhankhalid402 <subhan.khalid402@gmail.com>
2021-10-06 14:12:07 +0500

    musa&palmar image change

--- pages/musaandpalm.js
+++ pages/musaandpalm.js
@@ -112,12 +112,9 @@ export default function musaandpalm({textAnimationControls, handleBgColorChange,
 
                             <div>
                                 {
-                                    locale !== "ar" ?
-                                    <img className={styles.image2} src="/Musa&PalmEN2.png"
-                                         alt="Musa & Palm table"/>
-                                         :
-                                        <img className={styles.image2} src="/musa&palmar.png"
-                                             alt="Musa & Palm table"/>
+
+                                    <img className={styles.image2} src={locale !== "ar" ? "/Musa&PalmEN2.png" : "/musa&palmar.png"} alt="Musa & Palm table"/>
+
                                 }
                                 <span className={styles.para1}>{f("process.para1")}</span>
                             </div>
