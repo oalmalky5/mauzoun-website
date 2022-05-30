59180d79ef4111c543e8be7d0e361e772cb8fe44
oalmalky5 <oalmalky5@gmail.com>
2022-05-30 13:49:13 +0300

    completeing sta page

--- pages/sta.js
+++ pages/sta.js
@@ -96,42 +96,124 @@ export default function sta({ textAnimationControls, handleBgColorChange, handle
             <div className="container-content">
 
               <div className="celebrate">
-                <span class="celebrateTitle">{f("celebrate.title")}</span>
-                <span className={styles.para1}>{f("code.para1")}</span>
-                <span className={styles.para2}>{f("code.para2")}</span>
+                <span class="title">{f("celebrate.title")}</span>
+                <span class="para1">{f("celebrate.para1")}</span>
+                <span class="para2">{f("celebrate.para2")}</span>
               </div>
 
-              <div className="mm">
-                <span class="mmTitle">{f("mm.title")}</span>
-                <span className={styles.para1}>{f("mm.para1")}</span>
-                <span className={styles.para2}>{f("mm.para2")}</span>
+              <div className="fourtyEight">
+                <span class="title">{f("48.title")}</span>
+                <span class="para1">{f("48.para1")}</span>
+                <span class="para2">{f("48.para2")}</span>
+                <span class="para3">{f("48.para2")}</span>
               </div>
 
-              <div className="lang">
-                <span class="langTitle">{f("lang.title")}</span>
-                <span className={styles.para1}>{f("lang.para1")}</span>
+              <div className="visit">
+                <span class="title">{f("visit.title")}</span>
+                <span class="para1">{f("visit.para1")}</span>
+                <iframe
+                      className={styles.frame}
+                      width="600"
+                      height="400"
+                      src="https://www.instagram.com/p/CDELOpYHRYT/embed/"
+                      frameborder="0"
+                    ></iframe>
+                <span class="para2">{f("visit.para2")}</span>
 
-                <table>
-                  <tr>
-                    <th></th>
-                    <th class="arabic">{f("table.ara1")}</th>
-                  </tr>
-                  <tr>
-                    <td class="english">{f("table.eng")}</td>
-                    <td></td>
-                  </tr>
-                  <tr>
-                    <td></td>
-                    <td class="arabic">{f("table.ara2")}</td>
-                  </tr>
-                </table>
+                <section class="devCenter">
+                  <span class="package1">{f("visit.package1")}</span>
+                  <span class="package2">{f("visit.package2")}</span>
+                  <span class="package3">{f("visit.package3")}</span>
+                  <span class="package4">{f("visit.package4")}</span>
+                  <span class="package5">{f("visit.package5")}</span>
+                  <span class="package6">{f("visit.package6")}</span>
+                </section>
+                <iframe
+                      className={styles.frame}
+                      width="600"
+                      height="700"
+                      src="https://www.instagram.com/p/CElrHgAlwbB/embed/"
+                      frameborder="0"
+                    ></iframe>
+              </div>
 
-                <div className="story">
-                  <span class="storyTitle">{f("story.title")}</span>
-                  <WhiteBox decoratorsPositions={whiteBoxDecoratorsPositions}>
-                    <span className={styles.para1}>{f("story.para")}</span>
-                  </WhiteBox>
-                </div>
+              <div className="effort">
+                <span class="title">{f("effort.title")}</span>
+                <span class="para1">{f("effort.para1")}</span>
+
+                <section class="instaFrames">
+                    <iframe
+                        className={styles.frame}
+                        width="250"
+                        height="300"
+                        src="https://www.instagram.com/p/CB3cCMdFsTq/embed"
+                        frameborder="0"
+                  ></iframe>
+                  <iframe
+                        className={styles.frame}
+                        width="250"
+                        height="300"
+                        src="https://www.instagram.com/p/CDjra1EHkDC/embed"
+                        frameborder="0"
+                  ></iframe>
+                  <iframe
+                        className={styles.frame}
+                        width="250"
+                        height="300"
+                        src="https://www.instagram.com/p/CB3FuB7F_MB/embed"
+                        frameborder="0"
+                  ></iframe>
+                </section>
+              </div>
+
+              <div className="day">
+                <span class="title">{f("day.title")}</span>
+                <span class="para1">{f("day.para1")}</span>
+                <iframe
+                        className={styles.frame}
+                        width="600"
+                        height="700"
+                        src="https://www.instagram.com/tv/CFVQlSJHGmA/embed"
+                        frameborder="0"
+                  ></iframe>
+              </div>  
+
+              <div className="peopleContainer">
+                <span class="title">{f("people.title")}</span>
+                <span class="para1">{f("people.para1")}</span>
+
+                <section class="devCenter">
+                  <span class="people">{f("people.message1")}</span>
+                  <span class="people">{f("people.message2")}</span>
+                  <span class="people">{f("people.message3")}</span>
+                  <span class="people">{f("people.message4")}</span>
+                  <span class="people">{f("people.message5")}</span>
+                  <span class="peopleDiff">{f("people.message6")}</span>
+                </section>
+
+                <span class="para2">{f("people.para2")}</span>
+              </div>
+
+              <div className="pride">
+                <span class="title">{f("pride.title")}</span>
+                <span class="para1">{f("pride.para1")}</span>
+              </div>
+
+              <div className="words">
+                <span class="title">{f("words.title")}</span>
+                <span class="para1">{f("words.para1")}</span>
+                <iframe
+                        className={styles.frame}
+                        width="600"
+                        height="700"
+                        src="https://www.instagram.com/p/CF7W9ufl2aD/embed"
+                        frameborder="0"
+                  ></iframe>
+                <span class="para2">{f("words.para2")}</span>
+              </div>
+
+
+               
 
               </div>
 
@@ -139,7 +221,7 @@ export default function sta({ textAnimationControls, handleBgColorChange, handle
                             
             </div>
           </div>
-        </div>
+        
       </motion.div>
     </>
   );
