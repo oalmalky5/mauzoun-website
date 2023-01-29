9b9f8bcbfdf7ef3e1314a21d6777fec4c1e4b2af
oalmalky5 <oalmalky5@gmail.com>
2023-01-29 11:38:46 +0300

    removing under box para

--- pages/boutiquepublishing.js
+++ pages/boutiquepublishing.js
@@ -187,7 +187,7 @@ export default function BookCommissions({ textAnimationControls, handleBgColorCh
                           </WhiteBox>
                           
                         </div>
-                        {<div>{f("underBoxPara")}</div>}
+                        
                       </div>
                      
                     </>
