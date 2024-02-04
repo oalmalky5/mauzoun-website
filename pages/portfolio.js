89be6a898fc6f3e58b7b7aa3e3286c2020c69a2d
oalmalky5 <oalmalky5@gmail.com>
2024-02-04 16:40:48 +0300

    adding client logos

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,240 +1,280 @@
-import React from "react";
-import {useIntl} from "react-intl";
-import {useRouter} from "next/router";
-import {motion} from "framer-motion";
-import {Carousel} from "react-responsive-carousel";
-import {BsChevronLeft, BsChevronRight} from "react-icons/bs";
-import {NextSeo} from "next-seo";
-import styles from "../styles/portfolio.module.scss";
-import Menu from "../components/Menu";
-import formatJsxMessage from "../utils/formatJsxMessage";
-import WhiteBox from "../components/WhiteBox";
-import {MotionLogo} from "../components/MotionLogo"
-import ContactButton from "../components/ContactButton";
-import mauj from './mauj'
-import musaandpalmCopy from "./musaandpalm.js";
-import Footer from "../components/Footer";
+import React from 'react';
+import { useIntl } from 'react-intl';
+import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
+import { Carousel } from 'react-responsive-carousel';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import { NextSeo } from 'next-seo';
+import styles from '../styles/portfolio.module.scss';
+import Menu from '../components/Menu';
+import formatJsxMessage from '../utils/formatJsxMessage';
+import WhiteBox from '../components/WhiteBox';
+import { MotionLogo } from '../components/MotionLogo';
+import ContactButton from '../components/ContactButton';
+import mauj from './mauj';
+import musaandpalmCopy from './musaandpalm.js';
+import Footer from '../components/Footer';
 
-const backgroundColor = "#f7f5f0";
+const backgroundColor = '#f7f5f0';
 
 const clientLogos = [
-    "/clientLogos/1.png",
-    "/clientLogos/2.png",
-    "/clientLogos/3.png",
-    "/clientLogos/4.png",
-    "/clientLogos/5.png",
-    "/clientLogos/6.png"
+  '/clientLogos/1.png',
+  '/clientLogos/2.png',
+  '/clientLogos/3.png',
+  '/clientLogos/4.png',
+  '/clientLogos/5.png',
 ];
 
 const whiteBoxDecoratorsPositions = {
-    fromTop: [
-        {marginTop: "70px"},
-        {
-            preferredMargin: "450px",
-        },
-        {
-            preferredMargin: "467px",
-        },
-        {
-            preferredMargin: "484px",
-        },
-        {
-            preferredMargin: "501px",
-        },
-        {
-            marginTop: "45px",
-            preferredMargin: "735px",
-        },
-        {
-            marginTop: "62px",
-            preferredMargin: "735px",
-        },
-    ],
+  fromTop: [
+    { marginTop: '70px' },
+    {
+      preferredMargin: '450px',
+    },
+    {
+      preferredMargin: '467px',
+    },
+    {
+      preferredMargin: '484px',
+    },
+    {
+      preferredMargin: '501px',
+    },
+    {
+      marginTop: '45px',
+      preferredMargin: '735px',
+    },
+    {
+      marginTop: '62px',
+      preferredMargin: '735px',
+    },
+  ],
 };
 
-export default function portfolio({textAnimationControls, handleBgColorChange, handleOpenNav, history, isNavOpen, ...rest}) {
-    const locale = useRouter().locale;
-    const {key, initial, animate, variants} = rest;
+export default function portfolio({
+  textAnimationControls,
+  handleBgColorChange,
+  handleOpenNav,
+  history,
+  isNavOpen,
+  ...rest
+}) {
+  const locale = useRouter().locale;
+  const { key, initial, animate, variants } = rest;
 
-    const intl = useIntl();
-    const f = (id, options) =>
-        formatJsxMessage(intl, locale, id, {
-            shouldFade: true,
-            animationControls: textAnimationControls,
-            ...options,
-        });
+  const intl = useIntl();
+  const f = (id, options) =>
+    formatJsxMessage(intl, locale, id, {
+      shouldFade: true,
+      animationControls: textAnimationControls,
+      ...options,
+    });
 
-    const getClients = (category) => {
-        let i = 1;
-        let numberOfClients = Object.keys(intl.messages).filter((e) =>
-            e.startsWith(`${category}.clients.`)
-        ).length;
-        let clients = [];
+  const getClients = (category) => {
+    let i = 1;
+    let numberOfClients = Object.keys(intl.messages).filter((e) =>
+      e.startsWith(`${category}.clients.`)
+    ).length;
+    let clients = [];
 
-        while (i < numberOfClients + 1) {
-            clients.push(
-                <span
-                    key={`${category}.clients.${i}`}
-                    className={styles.client + " heading"}
-                    style={{
-                        fontWeight: numberOfClients !== 1 && i % 2 !== 0 ? "bold" : "normal",
-                    }}
-                >
+    while (i < numberOfClients + 1) {
+      clients.push(
+        <span
+          key={`${category}.clients.${i}`}
+          className={styles.client + ' heading'}
+          style={{
+            fontWeight:
+              numberOfClients !== 1 && i % 2 !== 0 ? 'bold' : 'normal',
+          }}
+        >
           {f(`${category}.clients.${i}`)}
         </span>
-            );
-            i += 1;
+      );
+      i += 1;
+    }
+    return clients;
+  };
+
+  const preferredSide = locale === 'ar' ? 'right' : 'left';
+  const oppositeSide = locale === 'ar' ? 'left' : 'right';
+
+  React.useEffect(() => handleBgColorChange(backgroundColor), []);
+  console.log('locale :: => ', locale);
+  return (
+    <>
+      <NextSeo
+        title={locale !== 'ar' ? 'Mauzoun | Portfolio' : 'مَوْزوْن | أعمالنا'}
+        description={
+          locale !== 'ar' ? 'Mauzoun | Portfolio' : 'مَوْزوْن | أعمالنا'
         }
-        return clients;
-    };
+      />
 
-    const preferredSide = locale === "ar" ? "right" : "left";
-    const oppositeSide = locale === "ar" ? "left" : "right";
+      <div className="background-animation" style={{ backgroundColor }} />
 
-    React.useEffect(() => handleBgColorChange(backgroundColor), []);
-    console.log("locale :: => ", locale)
-    return (
-        <>
-            <NextSeo
-                title={locale !== "ar" ? "Mauzoun | Portfolio" : "مَوْزوْن | أعمالنا"}
-                description={locale !== "ar" ? "Mauzoun | Portfolio" : "مَوْزوْن | أعمالنا"}
+      <motion.div
+        key={key}
+        initial={initial}
+        animate={animate}
+        variants={variants}
+      >
+        {/* <ContactButton isNavOpen={isNavOpen} history={history}/> */}
+        <div
+          style={{
+            position: 'fixed',
+            display: 'flex',
+            alignItems: 'stretch',
+            width: '100%',
+            height: '100%',
+            overflowX: 'hidden',
+            // overflowY: "scroll",
+          }}
+        >
+          <div
+            className="test-portfolio"
+            style={{
+              backgroundColor: backgroundColor,
+              width: '72%',
+              height: '150vh',
+              position: 'absolute',
+              zIndex: -1,
+            }}
+          ></div>
+          <div
+            className="bg-animation-portfolio"
+            style={{
+              position: 'relative',
+              width: '100%',
+              height: '100%',
+              zIndex: 8,
+              overflow: isNavOpen ? 'hidden' : null,
+            }}
+          >
+            <MotionLogo />
+            <Menu
+              backgroundColor={backgroundColor}
+              textAnimationControls={textAnimationControls}
+              isNavOpen={isNavOpen}
+              handleOpenNav={handleOpenNav}
             />
 
-            <div className="background-animation" style={{backgroundColor}}/>
+            <div className="container">
+              <div
+                className="container-background"
+                style={{ backgroundColor }}
+              ></div>
+              <div className="container-content">
+                <span className="title">{f('title')}</span>
 
-            <motion.div
-                key={key}
-                initial={initial}
-                animate={animate}
-                variants={variants}
+                <span className="mainHeader">{f('subtitle')}</span>
+              </div>
+              <div className="container-content">
+                <span style={{ marginBottom: '50px' }}>{f('content')}</span>
 
+                {/* Content Writing */}
+                <span className="titleMain">{f('contentWriting.title')}</span>
+                <span className="clientName">
+                  {getClients('contentWriting')}
+                </span>
 
-            >
-                {/* <ContactButton isNavOpen={isNavOpen} history={history}/> */}
-                <div
-                    style={{
-                        position: "fixed",
-                        display: "flex",
-                        alignItems: "stretch",
-                        width: "100%",
-                        height: "100%",
-                        overflowX: "hidden",
-                        // overflowY: "scroll",
-                    }}
-                >
+                <hr className="big-margin" size={1} color="black" />
+
+                {/* Creative Writing */}
+                <span className="titleMain">{f('creativeWriting.title')}</span>
+                {getClients('creativeWriting')}
+
+                <hr className="big-margin" size={1} color="black" />
+
+                {/* Boutique Publishing */}
+                <span className="titleMain">
+                  {f('boutiquePublishing.title')}
+                </span>
+                {getClients('boutiquePublishing')}
+              </div>
+
+              <div className="container-object">
+                <div className={styles.totalWhiteBox}>
+                  <WhiteBox
+                    style={{ marginTop: '50px' }}
+                    decoratorsPositions={whiteBoxDecoratorsPositions}
+                  >
+                    {f('clients')}
+
                     <div
-                        className="test-portfolio"
-                        style={{
-                            backgroundColor: backgroundColor,
-                            width: "72%",
-                            height: "150vh",
-                            position: "absolute",
-                            zIndex: -1,
-                        }}
-                    ></div>
-                    <div
-                        className="bg-animation-portfolio"
-                        style={{
-                            position: "relative",
-                            width: "100%",
-                            height: "100%",
-                            zIndex: 8,
-                            overflow: isNavOpen ? "hidden" : null,
-                        }}
+                      className={styles.carousel}
+                      style={{ direction: 'initial' }}
                     >
-                        <MotionLogo/>
-                        <Menu
-                            backgroundColor={backgroundColor}
-                            textAnimationControls={textAnimationControls}
-                            isNavOpen={isNavOpen}
-                            handleOpenNav={handleOpenNav}
-                        />
-
-                        <div className="container">
-                            <div className="container-background" style={{backgroundColor}}></div>
-                            <div className="container-content">
-                                <span className="title">{f("title")}</span>
-
-                                <span className="mainHeader">{f("subtitle")}</span>
+                      <Carousel
+                        showStatus={false}
+                        showIndicators={false}
+                        showThumbs={false}
+                        selectedItem={
+                          locale === 'ar' ? clientLogos.length - 1 : 0
+                        }
+                        renderArrowPrev={(clickHandler, hasPrev) =>
+                          hasPrev && (
+                            <div
+                              className={styles.arrowContainer}
+                              style={{
+                                [locale === 'en-US'
+                                  ? preferredSide
+                                  : oppositeSide]: 0,
+                              }}
+                              onClick={clickHandler}
+                            >
+                              <div
+                                className={styles.arrow}
+                                style={{
+                                  [locale === 'en-US'
+                                    ? preferredSide
+                                    : oppositeSide]: 0,
+                                }}
+                              >
+                                <BsChevronLeft className={styles.chevron} />
+                              </div>
                             </div>
-                            <div className="container-content">
-                                <span style={{marginBottom: "50px"}}>{f("content")}</span>
-
-                                {/* Content Writing */}
-                                <span className="titleMain">{f("contentWriting.title")}</span>
-                                <span className="clientName">{getClients("contentWriting")}</span>
-
-                                <hr className="big-margin" size={1} color="black"/>
-
-                                {/* Creative Writing */}
-                                <span className="titleMain">{f("creativeWriting.title")}</span>
-                                {getClients("creativeWriting")}
-
-                                <hr className="big-margin" size={1} color="black"/>
-
-                                {/* Boutique Publishing */}
-                                <span className="titleMain">{f("boutiquePublishing.title")}</span>
-                                {getClients("boutiquePublishing")}
+                          )
+                        }
+                        renderArrowNext={(clickHandler, hasNext) =>
+                          hasNext && (
+                            <div
+                              className={styles.arrowContainer}
+                              style={{
+                                [locale === 'en-US'
+                                  ? oppositeSide
+                                  : preferredSide]: 0,
+                              }}
+                              onClick={clickHandler}
+                            >
+                              <div
+                                className={styles.arrow}
+                                style={{
+                                  [locale === 'en-US'
+                                    ? oppositeSide
+                                    : preferredSide]: 0,
+                                }}
+                              >
+                                <BsChevronRight className={styles.chevron} />
+                              </div>
                             </div>
-
-                            <div className="container-object">
-                                <div className={styles.totalWhiteBox}>
-                                    <WhiteBox style={{marginTop: "50px"}}
-                                              decoratorsPositions={whiteBoxDecoratorsPositions}>
-                                        {f("clients")}
-
-                                        <div className={styles.carousel} style={{direction: "initial"}}>
-                                            <Carousel
-                                                showStatus={false}
-                                                showIndicators={false}
-                                                showThumbs={false}
-                                                selectedItem={locale === "ar" ? (clientLogos).length - 1 : 0}
-                                                renderArrowPrev={(clickHandler, hasPrev) =>
-                                                    hasPrev && (
-                                                        <div
-                                                            className={styles.arrowContainer}
-                                                            style={{[locale === "en-US" ? preferredSide : oppositeSide]: 0}}
-                                                            onClick={clickHandler}
-                                                        >
-                                                            <div
-                                                                className={styles.arrow}
-                                                                style={{[locale === "en-US" ? preferredSide : oppositeSide]: 0}}
-                                                            >
-                                                                <BsChevronLeft className={styles.chevron}/>
-                                                            </div>
-                                                        </div>
-                                                    )
-                                                }
-                                                renderArrowNext={(clickHandler, hasNext) =>
-                                                    hasNext && (
-                                                        <div
-                                                            className={styles.arrowContainer}
-                                                            style={{[locale === "en-US" ? oppositeSide : preferredSide]: 0}}
-                                                            onClick={clickHandler}
-                                                        >
-                                                            <div
-                                                                className={styles.arrow}
-                                                                style={{[locale === "en-US" ? oppositeSide : preferredSide]: 0}}
-                                                            >
-                                                                <BsChevronRight className={styles.chevron}/>
-                                                            </div>
-                                                        </div>
-                                                    )
-                                                }
-                                            >
-                                                {(locale === "ar" ? JSON.parse(JSON.stringify(clientLogos)).reverse() : clientLogos).map((e) => (
-                                                    <img src={e} priority="true"/>
-                                                ))}
-                                            </Carousel>
-                                        </div>
-                                    </WhiteBox>
-                                </div>
-                            </div>
-                        </div>
+                          )
+                        }
+                      >
+                        {(locale === 'ar'
+                          ? JSON.parse(JSON.stringify(clientLogos)).reverse()
+                          : clientLogos
+                        ).map((e) => (
+                          <img src={e} priority="true" />
+                        ))}
+                      </Carousel>
                     </div>
+                  </WhiteBox>
                 </div>
-            </motion.div>
-            <Footer/>
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <Footer />
+    </>
+  );
 }
