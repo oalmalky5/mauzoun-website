6c2d48ed4d9d1f9410b905a7678c10011103d1e2
oalmalky5 <oalmalky5@gmail.com>
2024-03-30 17:51:00 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   i18n: {
-    locales: ["en-US", "ar"],
-    defaultLocale: "en-US",
+    locales: ['en-US', 'ar'],
+    defaultLocale: 'en-US',
   },
   images: {
-    domains: ["i.imgur.com", "writingandwellness.com"],
+    domains: ['i.imgur.com', 'writingandwellness.com'],
   },
   async redirects() {
     return [
@@ -13,14 +13,13 @@ module.exports = {
         destination: '/story',
         permanent: true, // set to false if you want the redirect to be temporary
       },
-    ]
+    ];
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      // Configure the fallback for the 'fs' module when running in the browser
-      config.resolve.fallback = { fs: false };
+      config.resolve.fallback = { fs: false }; // Configure fallback for 'fs' module in the browser
     }
-
     return config;
   },
+  output: 'standalone', // Add this line for Next.js 12+ or 'export' for static HTML export
 };
